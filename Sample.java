8b07043a1a827d477d0c82eb07edb830457f7dc9
Shota-Gejima <bewith.sho@gmail.com>
2023-12-22 16:40:45 +0900

    push2

--- Sample.java
+++ Sample.java
@@ -2,5 +2,6 @@ public class Sample{
   public static void main(String[] args){
     System.out.println("Hello World");
     System.out.println("Hello java!");
+    System.out.println("push");
   }
 }
