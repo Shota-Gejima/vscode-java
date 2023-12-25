0be82bb7c7cc378eeeb90be994cc383d6f8b2031
Shota-Gejima <bewith.sho@gmail.com>
2023-12-25 09:29:42 +0900

    [first]lesson6

---
+++ lesson6/Main.java
@@ -0,0 +1,12 @@
+public class Main{
+  public static void main(String[] args) {
+    dowarusa();
+    dokogame();
+    callDeae();
+    showMondokoro();
+
+  }
+  public static void dowarusa() {
+    System.out.println("きなこでござる。食えませんがの。");
+  }
+}
