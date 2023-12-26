173f295ca98d01feee69c2c91b83edac623eb2ce
Shota-Gejima <bewith.sho@gmail.com>
2023-12-26 18:49:14 +0900

    [finsh]lesson9

---
+++ lesson9/Thief.java
@@ -0,0 +1,19 @@
+public class Thief{
+  String name;
+  int hp = 40;
+  int mp = 5;
+
+  public Thief(String name, int hp, int mp){
+    this.name = name;
+    this.hp = hp;
+    this.mp = mp;
+  }
+
+  public Thief(String name, int hp){
+    this(name, hp, 5);
+  }
+
+  public Thief(String name){
+    this(name, 40);
+  }
+}
