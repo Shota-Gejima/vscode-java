e4436cf566304e058881e6f1faca5360aa301bd1
Shota-Gejima <bewith.sho@gmail.com>
2023-12-29 10:38:46 +0900

    [second]finish chapter8

--- chapter8/Hero.java
+++ chapter8/Hero.java
@@ -1,27 +1,30 @@
-public class Hero{
+public class Hero {
   String name;
   int hp;
 
-  public void attack(){
-
-  }
   public void sleep(){
     this.hp = 100;
-    System.out.println(this.name + "は、眠って回復した！");
+    System.out.println(this.name + "は、眠って回復した");
+    System.out.println("残り" + this.hp);
   }
+
   public void sit(int sec){
     this.hp += sec;
-    System.out.println(this.name + "は"+ sec + "秒座った!");
-    System.out.println("HPが" + sec + "ポイント回復した");
+    System.out.println(this.name + "は、" + sec + "秒座った!");
+    System.out.println(this.name + "は、" + sec + "ポイント回復した");
+    System.out.println("残り" + this.hp);
   }
+
   public void slip(){
     this.hp -= 5;
     System.out.println(this.name + "は、転んだ！");
     System.out.println("5のダメージ！");
+    System.out.println("残り" + this.hp);
   }
+
   public void run(){
     System.out.println(this.name + "は、逃げ出した！");
-    System.out.println("GAME OVER");
-    System.out.println("最終的なhpは" + this.hp + "でした");  
+    System.out.println("GAMEOVER");
+    System.out.println("最終hpは、" + this.hp +"でした。");
   }
 }
--- chapter8/Matango.java
+++ chapter8/Matango.java
@@ -3,7 +3,7 @@ public class Matango{
   final int LEVEL = 10;
   char suffix;
 
-  public void run() {
-    System.out.println("お化けキノコ" + this.suffix + "は、逃げ出した！");
+  public void run(){
+    System.out.println("お化けキノコ" + this.suffix + "は逃げ出しました！");
   }
 }
