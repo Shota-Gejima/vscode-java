7e366c19324f2176a6724b01d18f548378d09cde
Shota-Gejima <bewith.sho@gmail.com>
2024-01-01 14:07:18 +0900

    [fiinish]lesson11-2

--- lesson11/TangibleAsset.java
+++ lesson11/TangibleAsset.java
@@ -1,20 +1,12 @@
-public abstract class TangibleAsset {
-  String name;
-  int price;
+public abstract class TangibleAsset extends Asset {
+
   String color;
 
   public TangibleAsset(String name, int price, String color){
-    this.name = name;
-    this.price = price;
+    super(name, price);
     this.color = color;
   }
 
-  public String getName(){
-    return this.name;
-  }
-  public int getPrice(){
-    return this.price;
-  }
   public String getColor(){
     return this.color;
   }
