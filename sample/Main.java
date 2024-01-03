568b752c181a6bbeaacd5ae9b29e0ef1b9e7ba36
Shota-Gejima <bewith.sho@gmail.com>
2024-01-03 23:32:46 +0900

    [second]finsh sample

---
+++ sample/Main.java
@@ -0,0 +1,9 @@
+package sample;
+
+public class Main {
+  public static void main(String[] args){
+    Thief t1 = new Thief("アサカ", 40, 5);
+    Thief t2 = new Thief("ミナト", 100);
+    Thief t3 = new Thief("スガワラ"); 
+  }
+}
--- sample/Thief.java
+++ sample/Thief.java
@@ -3,4 +3,19 @@ public class Thief{
   String name;
   int hp;
   int mp;
+
+  public Thief(String name, int hp, int mp){
+    this.name = name;
+    this.hp = hp;
+    this.mp = mp;
+    System.out.println("私の名前は" + name + "です。hpは" + hp + "で、mpは" + mp + "です。");
+  }
+
+  public Thief(String name, int hp){
+    this(name, hp, 5);
+  }
+
+  public Thief(String name){
+    this(name, 40, 5);
+  }
 }
