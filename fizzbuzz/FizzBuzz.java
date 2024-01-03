006fff011f9be0983d9cd97060869e147654ff1e
Shota-Gejima <bewith.sho@gmail.com>
2024-01-04 00:09:57 +0900

    [first]FizzBuzz-2

--- fizzbuzz/FizzBuzz.java
+++ fizzbuzz/FizzBuzz.java
@@ -1,15 +1,15 @@
 public class FizzBuzz{
-  public static void useForLoop(int end) {
-    for (int i=1; i<=end; i++) {
-        if (i%3==0 && i%5==0) {
+  public static void useForLoop() {
+    System.out.println("好きな数字を入力して下さい。");
+    int end = new java.util.Scanner(System.in).nextInt();
+        if (end%3==0 && end%5==0) {
             System.out.println("Fizz Buzz");
-        } else if (i%3==0) {
+        } else if (end%3==0) {
             System.out.println("Fizz");
-        } else if (i%5==0) {
+        } else if (end%5==0) {
             System.out.println("Buzz");
         } else {
-            System.out.println(i);
+            System.out.println("どれにも当てはまりません");
         }
-    }
   }
 }
--- fizzbuzz/Main.java
+++ fizzbuzz/Main.java
@@ -1,6 +1,6 @@
 public class Main {
   public static void main(String[] args){
     FizzBuzz fb = new FizzBuzz();
-    fb.useForLoop(15);
+    fb.useForLoop();
   }
 }
