80ccf3a5e1d34e9d085370596c5b7b434640ac6f
Shota-Gejima <bewith.sho@gmail.com>
2024-01-05 18:20:22 +0900

    [finsh]FizzBuzz iが50になるまでループ

--- fizzbuzz/FizzBuzz.java
+++ fizzbuzz/FizzBuzz.java
@@ -11,6 +11,17 @@ public class FizzBuzz{
     } else {
         System.out.println("どれにも当てはまりません");
     }
-    
+    // iが50になるまでループ
+    for (int i = 1; i <= 50; i++){
+      if (i % (3 * 5) == 0){
+        System.out.println("fizzBuzz");
+      } else if(i % 3 == 0){
+        System.out.println("Fizz");
+      } else if (i % 5 == 0){
+        System.out.println("Buzz");
+      } else {
+        System.out.println(i);
+      }
+    }
   }
 }
