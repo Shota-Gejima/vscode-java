2b945b30c8246fe9ca288a5f68fb3bfc96eb566e
Shota-Gejima <bewith.sho@gmail.com>
2024-01-05 18:19:28 +0900

    [first] FizzBuzz好きな数字

--- fizzbuzz/FizzBuzz.java
+++ fizzbuzz/FizzBuzz.java
@@ -2,14 +2,15 @@ public class FizzBuzz{
   public static void useForLoop() {
     System.out.println("好きな数字を入力して下さい。");
     int end = new java.util.Scanner(System.in).nextInt();
-        if (end%3==0 && end%5==0) {
-            System.out.println("Fizz Buzz");
-        } else if (end%3==0) {
-            System.out.println("Fizz");
-        } else if (end%5==0) {
-            System.out.println("Buzz");
-        } else {
-            System.out.println("どれにも当てはまりません");
-        }
+    if (end%3==0 && end%5==0) {
+        System.out.println("Fizz Buzz");
+    } else if (end%3==0) {
+        System.out.println("Fizz");
+    } else if (end%5==0) {
+        System.out.println("Buzz");
+    } else {
+        System.out.println("どれにも当てはまりません");
+    }
+    
   }
 }
