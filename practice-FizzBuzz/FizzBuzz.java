804c969c2e6980150d2528fbd96f453dabb63fb8
Shota-Gejima <bewith.sho@gmail.com>
2024-01-06 17:49:34 +0900

    [finish]practice-FizzBuzz

--- practice-FizzBuzz/FizzBuzz.java
+++ practice-FizzBuzz/FizzBuzz.java
@@ -1,5 +1,6 @@
 public class FizzBuzz {
   public static void main(String[] args){
+    // 好きな数字
     System.out.println("好きな数字を入力してください");
     int num = new java.util.Scanner(System.in).nextInt();
     if (num%(3*5)==0){
@@ -11,7 +12,7 @@ public class FizzBuzz {
     } else {
       System.out.println(num + "はどれにも当てはまりません");
     }
-
+    // 100までループ
     for (int i=1; i<=100; i++){
       if (i%(3*5)==0){
         System.out.println("FizzBuzz"+"  "+i);
@@ -23,6 +24,21 @@ public class FizzBuzz {
         System.out.println(i);
       }
     }
+    // 3回好きな数字をループで回す
+    for (int s=1; s<=3; s++){
+      System.out.println("あと"+(4-s)+"回");
+      System.out.println("好きな数字を入力してください");
+      int numLike = new java.util.Scanner(System.in).nextInt();
+      if (numLike%(3*5)==0){
+        System.out.println("FizzBuzz");
+      } else if (numLike%3==0){
+        System.out.println("Fizz");
+      } else if (numLike%5==0){
+        System.out.println("Buzz");
+      } else {
+        System.out.println(numLike);
+      }
+    }
   }
-  
+
 }
