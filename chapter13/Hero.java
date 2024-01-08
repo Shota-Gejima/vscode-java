ff8c160179b2bbc1394e01ba625e4dcb7f0ca979
Shota-Gejima <bewith.sho@gmail.com>
2024-01-08 17:29:14 +0900

    [first] getter setter

--- chapter13/Hero.java
+++ chapter13/Hero.java
@@ -1,2 +1,11 @@
 public class Hero{
+  private String name;
+
+  public String getName(){
+    return this.name;
+  }
+  public void setName(String name){
+    this.name = name;
+  }
+
 }
--- chapter13/Main.java
+++ chapter13/Main.java
@@ -1,5 +1,12 @@
 public class Main{
   public static void main(String[] args){
-    
+    Hero h = new Hero();
+    // Heroクラスでnameをprivate宣言しているためフィールドに直接代入できない
+    // h.name = "ミナト";
+    // System.out.println(h.name);
+
+    // Heroクラスのセッターメソッドで代入し、ゲッターメソッドで呼び出す
+    h.setName("すがわら");
+    System.out.println(h.getName());
   }
 }
