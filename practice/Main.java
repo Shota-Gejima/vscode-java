97d5909f5ad94ec04b71bae9609a95e5985ccdef
Shota-Gejima <bewith.sho@gmail.com>
2024-01-17 13:41:11 +0900

    practice

--- practice/Main.java
+++ practice/Main.java
@@ -1,17 +1,8 @@
-package practice;
-public class Main{
+public class Main {
   public static void main(String[] args){
-
-    Cleric c = new Cleric();
-    c.selfAid();
-    int ans = c.pray(2);
-    System.out.println("実際に回復した量は" + ans + "ポイントで、最終mp数は" + c.mp );
-
-    public static void main(String[] args) throws Exception {
-      Zenhan.doWarusa();
-      Zenhan.doTogame();
-      comment.Kouhan.callDeae();
-      comment.Kouhan.showMondokoro();
-  
+    FizzBuzz use = new FizzBuzz();
+    use.game();
+    use.fornum();
   }
 }
+;
