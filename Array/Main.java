2ac7a1560810e65efb383633f1278b54adfa737d
Shota-Gejima <bewith.sho@gmail.com>
2024-01-19 14:48:11 +0900

    [復習]配列

--- Array/Main.java
+++ Array/Main.java
@@ -1,18 +1,10 @@
 public class Main{
   public static void main(String[] args){
-    int [] heights = {172, 149, 152, 191, 155};
-    java.util.Arrays.sort(heights);
-    for (int i: heights){
-      System.out.println(i);
+    int [] point = {100,200,300};
+
+    for (int num : point){
+      System.out.println(num);
     }
 
-    int[] num = new int[2];
-    num[0] = 10;
-    num[1] = 20;
-
-    int result = num[0] + num[1];
-    System.out.println(result);
   }
-
-
 }
