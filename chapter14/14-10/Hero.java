283e7253716ad249d47e5962e5546a37971613c7
Shota-Gejima <bewith.sho@gmail.com>
2024-01-20 17:34:37 +0900

    [first] static制約

--- chapter14/14-10/Hero.java
+++ chapter14/14-10/Hero.java
@@ -5,6 +5,8 @@ public class Hero {
 
   public static void setRandomMoney(){
     Hero.money = (int)(Math.random() * 1000);
+    // ↓以下はstatic付いていないフィールドを使用しようとしているためコンパイル
+    // System.out.println(this.name + "たちの所持金を初期化しました");
   }
 
 }
