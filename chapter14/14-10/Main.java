7cfe1f126f3170b486c35acfe97e64f3c74299f0
Shota-Gejima <bewith.sho@gmail.com>
2024-01-20 16:45:14 +0900

    [first] インスタンスなし

--- chapter14/14-10/Main.java
+++ chapter14/14-10/Main.java
@@ -1,11 +1,6 @@
 public class Main {
   public static void main(String[] args) {
-    Hero h1 = new Hero();
-    Hero h2 = new Hero();
-    h1.hp = 100;
-    System.out.println(h1.hp);
+    Hero.money = 100;
     System.out.println(Hero.money);
-    h1.money = 300;
-    System.out.println(h2.money);
   }
 }
