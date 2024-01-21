c2ae8a1ecb4eee1246e8253978f91e871dde97d6
Shota-Gejima <bewith.sho@gmail.com>
2024-01-21 18:27:58 +0900

    [復習]2-11

--- chapter2/Main.java
+++ chapter2/Main.java
@@ -1,6 +1,8 @@
 public class Main {
   public static void main(String[] args){
     System.out.println("私の好きな記号は二重引用付(\")です");
+    int m = Math.max(100,200);
+    System.out.println(m);
   }
   
 }
