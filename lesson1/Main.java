b3e15dbcc372a6b6b131b066d7ec0061f41cdbc9
Shota-Gejima <bewith.sho@gmail.com>
2024-01-21 17:53:10 +0900

    [復習]lesson1

---
+++ lesson1/Main.java
@@ -0,0 +1,16 @@
+public class Main{
+  public static void main(String[] args){
+    // 1-2
+    int a = 3;
+    int b = 5;
+    int c = a*b;
+    System.out.println("a * b =" + c);
+
+    // 1-3
+    boolean result = true;
+    char favoriteCharacter = '駆';
+    double pi = 3.14;
+    long number = 314159265853979L;
+    String msg = "ミナトの攻撃！敵に15ポイントのダメージを与えた。";
+  }
+}
