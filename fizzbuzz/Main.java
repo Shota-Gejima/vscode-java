6590b1b54ab0ecf9c93bff9eb225fc035a2d52aa
Shota-Gejima <bewith.sho@gmail.com>
2024-01-22 09:47:14 +0900

    [復習]FizzBuzz

--- fizzbuzz/Main.java
+++ fizzbuzz/Main.java
@@ -1,7 +1,23 @@
 public class Main {
   public static void main(String[] args){
-    FizzBuzz2 fb2 = new FizzBuzz2();
-    fb2.use();
-    fb2.ForLoop();
+    System.out.println("好きな数字入力してください");
+    int num = new java.util.Scanner(System.in).nextInt();
+    if (num%(3*5)==0){
+      System.out.println("FizzBuzz");
+    } else if(num%3==0){
+      System.out.println("Fizz");
+    } else if (num%5==0){
+      System.out.println("Buzz");
+    } else{ System.out.println(num); }
+
+    for (int i=0; i<=100; i++){
+      if (i%(3*5)==0){
+        System.out.println("FizzBuzz"+"("+i+")");
+      } else if (i%3==0){
+        System.out.println("Fizz"+"("+i+")");
+      } else if (i%5==0){
+        System.out.println("Buzz"+"("+i+")");
+      } else {System.out.println(i);}
+    }
   }
 }
