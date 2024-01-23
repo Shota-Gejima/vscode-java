8ceb1e9b6f3dd9d389bf186146a8210adefd824b
Shota-Gejima <bewith.sho@gmail.com>
2024-01-23 09:46:53 +0900

    [復習]lesson4-2

--- lesson4/Main.java
+++ lesson4/Main.java
@@ -1,26 +1,30 @@
 public class Main {
   public static void main(String[] args){
     // 練習4-1
-    int [] points = {10,20,30,40};
-    double [] weight = {1.1,2.2,3.3,4.4};
-    boolean [] answers = {true,false,true,false};
-    String [] names = {"あ","い","う"};
+    int[] points = new int[4];
+    double [] weights = new double[5];
+    boolean [] answers = new boolean[3];
+    String [] names = new String [3];
     // 練習4-2
-    int[] moneyList = {121902,8302,55100};
-    for (int i=0; i<moneyList.length; i++){
-      System.out.print(moneyList[i]+" ");
+    int [] moneyList = new int[3];
+    moneyList[0] = 121902;
+    moneyList[1] = 8302;
+    moneyList[2] = 55100;
+    for (int m=0; m<moneyList.length; m++){
+      System.out.println(moneyList[m] + " ");
+      System.out.println(" ");
     }
-    for (int i:moneyList){
-      System.out.print(i+" ");
+    for (int m: moneyList){
+      System.out.print(m + " ");
     }
     // 練習4-4
-    int[] numbers = {3,4,9};
+    int [] numbers = {3,4,9};
     System.out.println("1桁の数字を入力してください");
     int input = new java.util.Scanner(System.in).nextInt();
-    for (int i : numbers){
-      if (input == i){
-        System.out.println("あたり");
-      } 
+    for (int num: numbers){
+      if (num==input){
+        System.out.println("アタリ");
+      } else{System.out.println("はずれ");}
     }
   }
 }
