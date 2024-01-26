49d98449fb675adb3e175b1022416db79e78b7d9
Shota-Gejima <bewith.sho@gmail.com>
2024-01-26 15:43:56 +0900

    [練習]要素

--- practice2/Main.java
+++ practice2/Main.java
@@ -1,36 +1,23 @@
 public class Main{
   public static void main(String[] args){
-    int [] num = new int[20];
-    for (int i=0; i<num.length; i++){
-      num[i] = i * 5;
+    int [] num = new int[10];
+    for (int i = 0; i<num.length; i++){
+      num [i] = i+1;
     }
-    for (int i=0; i<num.length; i++){
-      System.out.print(num[i]);
-      if (i<20-1){
-        System.out.print(",");
-      }
+    for (int i: num){
+      System.out.print(i+",");
     }
     System.out.println();
-    // 奇数
-    System.out.print("奇数:");
-    for (int i = 0; i<=10; i++){
-      if (num[i]%2!=0){
-        System.out.print(num[i]);
-        if (i<10-1){
-          System.out.print(",");
-        }
-      }
-    }
-    System.out.println();
-    // 偶数
-    System.out.print("偶数:");
-    for (int i = 11; i <= 19; i++){
+    int counts =0;
+    for (int i = 0; i<num.length; i++){
       if (num[i]%2==0){
         System.out.print(num[i]);
-        if (i<19-1){
+        counts++;
+        if (i<num.length-1){
           System.out.print(",");
         }
       }
     }
+    System.out.println("偶数の数は"+counts+"個です");
   }
 }
