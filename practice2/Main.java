f2a0d7df91136d823e3ce3a38e2f6ea756c0643f
Shota-Gejima <bewith.sho@gmail.com>
2024-01-26 15:31:20 +0900

    [復習]偶数奇数

--- practice2/Main.java
+++ practice2/Main.java
@@ -1,9 +1,36 @@
 public class Main{
   public static void main(String[] args){
-    int [] heights = {172, 149, 152, 191, 155};
-    java.util.Arrays.sort(heights);
-    for (int i: heights){
-      System.out.println(i);
+    int [] num = new int[20];
+    for (int i=0; i<num.length; i++){
+      num[i] = i * 5;
+    }
+    for (int i=0; i<num.length; i++){
+      System.out.print(num[i]);
+      if (i<20-1){
+        System.out.print(",");
+      }
+    }
+    System.out.println();
+    // 奇数
+    System.out.print("奇数:");
+    for (int i = 0; i<=10; i++){
+      if (num[i]%2!=0){
+        System.out.print(num[i]);
+        if (i<10-1){
+          System.out.print(",");
+        }
+      }
+    }
+    System.out.println();
+    // 偶数
+    System.out.print("偶数:");
+    for (int i = 11; i <= 19; i++){
+      if (num[i]%2==0){
+        System.out.print(num[i]);
+        if (i<19-1){
+          System.out.print(",");
+        }
+      }
     }
   }
 }
