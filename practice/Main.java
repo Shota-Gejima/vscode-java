980ab1139ba0a2b559c587728f669fc11e047cb2
Shota-Gejima <bewith.sho@gmail.com>
2024-01-27 18:37:57 +0900

    [復習]問題3

--- practice/Main.java
+++ practice/Main.java
@@ -17,6 +17,7 @@ public class Main {
       System.out.println("1~9の好きな数じを入力してください");
       int num = new Scanner(System.in).nextInt();
       array[i] = num;
+      if (array.length-(i+1)!=0)
       System.out.println("残り"+(array.length-(i+1))+"個");
     }
     int total=0;
@@ -26,5 +27,21 @@ public class Main {
     // キャスト演算子
     double avg = (double)total/array.length;
     System.out.println(array.length+"合計は"+total+"で、平均は"+avg);
+    // 問題3 以下の文字列型の配列が与えられたとき、配列内の文字列の中で最も長い文字列を表示するJavaのプログラムを書いてください。
+    // String[] words = {"apple", "banana", "kiwi", "strawberry", "grape"};
+    String[] words = {"apple", "banana", "kiwi", "strawberry", "grape"};
+    String longest = "";
+    for (String word: words){
+      if (word.length()>longest.length()){
+        longest = word;
+      }
+    }
+    System.out.println(longest+longest.length());
+    for (int i=0; i<words.length; i++){
+      if (words[i].length()>longest.length()){
+        longest = words[i];
+      }
+    }
+    System.out.println(longest);
   }
 }
