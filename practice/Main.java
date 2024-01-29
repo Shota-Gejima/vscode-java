7ac40a8eec11c9ed13931f916669244b86d539ce
Shota-Gejima <bewith.sho@gmail.com>
2024-01-29 16:15:05 +0900

    [復習]

--- practice/Main.java
+++ practice/Main.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 public class Main {
   public static void main(String[] args){
+
     // 問題1 以下の整数型の配列が与えられたとき、配列内の奇数の合計を計算して表示するJavaのプログラムを書いてください。
     // int[] numbers = {1, 5, 8, 3, 6, 9, 7, 2, 4, 11};
     int [] numbers = new int []{1, 5, 8, 3, 6, 9, 7, 2, 4, 11};
@@ -14,36 +15,30 @@ public class Main {
     System.out.println(totalNum);
     
     // 問題2 ユーザーに10個の整数を入力させ、それらの整数の合計と平均を計算して表示するJavaのプログラムを書いてください。
-    int[] array = new int[10];
+    int array[] = new int[10];
+    int totalArray = 0;
+    double avg = 0;
     for (int i=0; i<array.length; i++){
-      System.out.println("1~9の好きな数じを入力してください");
-      int num = new Scanner(System.in).nextInt();
-      array[i] = num;
-      if (array.length-(i+1)!=0)
-      System.out.println("残り"+(array.length-(i+1))+"個");
+      System.out.println("好きな数字を入力してください。あと"+(array.length-(i))+"回");
+      array[i] = new Scanner(System.in).nextInt();
     }
-    int total=0;
-    for (int i: array){
-      total += i;
+    for (int m: array){
+      totalArray += m;
     }
-    // キャスト演算子
-    double avg = (double)total/array.length;
-    System.out.println(array.length+"合計は"+total+"で、平均は"+avg);
+    avg = (double)totalArray/array.length;
+    System.out.println("合計は"+totalArray+"平均は"+avg);
+
     // 問題3 以下の文字列型の配列が与えられたとき、配列内の文字列の中で最も長い文字列を表示するJavaのプログラムを書いてください。
     // String[] words = {"apple", "banana", "kiwi", "strawberry", "grape"};
+    
     String[] words = {"apple", "banana", "kiwi", "strawberry", "grape"};
-    String longest = "";
-    for (String word: words){
-      if (word.length()>longest.length()){
-        longest = word;
-      }
-    }
-    System.out.println(longest+longest.length());
+    String word = "";
     for (int i=0; i<words.length; i++){
-      if (words[i].length()>longest.length()){
-        longest = words[i];
+      if (words[i].length() > word.length()){
+        word = words[i];
       }
     }
-    System.out.println(longest);
+    System.out.println(word);
+    
   }
 }
