d27c219b4001a0f8c407172d6c14243884748ed6
Shota-Gejima <bewith.sho@gmail.com>
2024-01-31 18:22:00 +0900

    [復習]lesson8

--- lesson8/Cleric.java
+++ lesson8/Cleric.java
@@ -1,27 +1,30 @@
 import java.util.*;
 public class Cleric {
   String name;
-  int hp;
+  int hp = 50;
   final int MAX_HP = 50;
-  int mp;
+  int mp =10;
   final int MAX_MP = 10;
 
   public void dmg(){
     this.hp -= 30;
-    System.out.println("聖職者のhpは"+this.hp+"になった");
+    System.out.println("聖職者のhpが"+this.hp+"になった");
   }
+
   public void selfAid(){
     this.mp -= 5;
     this.hp = this.MAX_HP;
-    System.out.println("mpを5減らすことで"+this.MAX_HP+"まで回復した");
+    System.out.println("mpが"+this.mp+"になったが聖職者のhpが"+MAX_HP+"になった");
   }
 
   public int pray(int sec){
-    System.out.println(sec+"秒間祈った");
+    // 理論上の回復量
     int recovery = new Random().nextInt(3)+sec;
-    System.out.println(recovery);
+    System.out.println("理論上の回復量が"+recovery);
+    // 実際の回復量
     int realRecovery = Math.min(MAX_MP-this.mp, recovery);
     this.mp += realRecovery;
     return realRecovery;
   }
+
 }
