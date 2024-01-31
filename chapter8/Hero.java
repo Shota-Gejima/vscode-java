0df3d967071d38449b3ef5693f7ae1d0c80078cf
Shota-Gejima <bewith.sho@gmail.com>
2024-01-31 11:35:47 +0900

    [復習]8-3

--- chapter8/Hero.java
+++ chapter8/Hero.java
@@ -8,8 +8,24 @@ public class Hero {
   int level = 10;
   // 操作の定義
   public void attack(){}
-  public void sleep(){}
-  public void sit(int sec){}
-  public void slip(){}
-  public void run(){}
+  public void sleep(){
+    this.hp =100;
+    System.out.println(this.name+"は眠って回復した");
+  }
+  public void sit(int sec){
+    this.hp += sec;
+    System.out.println(this.name+"は"+sec+"秒座った");
+    System.out.println("hpが"+sec+"ポイント回復した");
+  }
+  public void slip(){
+    this.hp -= 5;
+    System.out.println(this.name+"は転んだ");
+    System.out.println("5のダメージ");
+  }
+  public void run(){
+    System.out.println(this.name+"は、逃げ出した");
+    System.out.println("GAME OVER");
+    System.out.println("最終HPは"+this.hp+"でした");
+  }
 }
+// フィールドを用いるときは明示的にthisをつける
