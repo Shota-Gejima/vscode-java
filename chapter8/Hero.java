ee5b98d5bd6e9f63cdb8f303771d6bc8400550b4
Shota-Gejima <bewith.sho@gmail.com>
2024-01-31 11:22:07 +0900

    [復習]8-2

---
+++ chapter8/Hero.java
@@ -0,0 +1,15 @@
+package chapter8;
+
+public class Hero {
+  // 属性の定義
+  // フィールドの追加
+  String name;
+  int hp;
+  int level = 10;
+  // 操作の定義
+  public void attack(){}
+  public void sleep(){}
+  public void sit(int sec){}
+  public void slip(){}
+  public void run(){}
+}
