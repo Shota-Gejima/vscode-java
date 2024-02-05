650aefde1c3203fe89479919a71784985809d763
Shota-Gejima <bewith.sho@gmail.com>
2024-02-05 17:23:39 +0900

    [復習]問題2

--- practice/Main.java
+++ practice/Main.java
@@ -28,18 +28,20 @@ public class Main {
     System.out.println();
     System.out.println(numbersAdd);
     // 問題2 ユーザーに10個の整数を入力させ、それらの整数の合計と平均を計算して表示するJavaのプログラムを書いてください。
-    int array[] = new int[10];
-    int totalArray = 0;
-    double avg = 0;
-    for (int i=0; i<array.length; i++){
-      System.out.println("好きな数字を入力してください。あと"+(array.length-(i))+"回");
-      array[i] = new Scanner(System.in).nextInt();
+    int[] num = new int[10];
+    int total=0;
+    double avg =0;
+    for (int i=0; i<num.length; i++){
+      if (i!=9){
+        System.out.println("あと"+(9-i)+"回入力してください");
+        num[i] = new Scanner(System.in).nextInt();
+      }
     }
-    for (int m: array){
-      totalArray += m;
+    for (int i=0; i<num.length; i++){
+      total += num[i];
+      avg = (double)total/num.length;
     }
-    avg = (double)totalArray/array.length;
-    System.out.println("合計は"+totalArray+"平均は"+avg);
+    System.out.println("合計は"+total+"で、平均は"+avg);
 
     // 問題3 以下の文字列型の配列が与えられたとき、配列内の文字列の中で最も長い文字列を表示するJavaのプログラムを書いてください。
     // String[] words = {"apple", "banana", "kiwi", "strawberry", "grape"};
