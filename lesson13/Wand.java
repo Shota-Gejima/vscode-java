3ef854ffae3547434dde69b37be0bd2b028872f0
Shota-Gejima <bewith.sho@gmail.com>
2024-02-12 18:47:38 +0900

    [復習] first lesson13

--- lesson13/Wand.java
+++ lesson13/Wand.java
@@ -1,20 +1,4 @@
-package lesson13;
-
-public class Wand{
+public class Wand {
   private String name;
   private double power;
-
-  public String getName(){return this.name;}
-  public void setName(String name){
-    if (name == null || name.length() < 3){
-      throw new IllegalArgumentException("名前が適切ではありません");
-    } else {this.name = name;}
-    
-  }
-  public double getPower(){return this.power;}
-  public void setPower(double power){
-    if (power < 0.5 || power > 100 ){
-      throw new IllegalArgumentException("杖による増減幅が適切ではありません");
-    } else {this.power = power;}
-  }
 }
--- lesson13/Wizard.java
+++ lesson13/Wizard.java
@@ -1,41 +1,14 @@
-package lesson13;
-
 public class Wizard {
-  private int hp;
-  private int mp;
-  private String name;
-  private Wand wand;
-
-
-  public void heal(Hero h){
+  int hp;
+  int mp;
+  String name;
+  Wand wand;
+  void heal(Hero h){
     int basePoint = 10;
     int recovPoint =
-      (int)(basePoint * this.wand.getPower());
+      (int)(basePoint * this.wand.power);
     h.setHp(h.getHp() + recovPoint);
-    System.out.println(h.getName() + "HPのを" + recovPoint + "回復した!");
-  }
+    System.out.println(h.getName()+"のHPを"+recovPoint+"回復した!");
 
-  public void setHp(int hp){
-    if (hp < 0){this.hp = 0;} else {this.hp = hp;}
   }
-  public int getHp(){return this.hp;}
-  public void setMp( int mp){
-    if (mp < 0){
-      throw new IllegalArgumentException("mpの値が異常です");
-    } else {this.mp = mp;}
-  }
-  public int getMp(){ return this.mp;}
-  public void setName(String name){
-    if (name == null || name.length() < 3){
-      throw new IllegalArgumentException("魔法使いの名前が以上です");
-    }
-    this.name = name; }
-  public String getName(){ return this.name; }
-  public void setWand(Wand wand){
-    if (wand == null){
-      throw new IllegalArgumentException("杖がnullです");
-    }
-    this.wand = wand; 
-  }
-  public Wand getWand(){ return this.wand; }
 }
