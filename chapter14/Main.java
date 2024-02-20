72db25641f88e9979279b18c389676deb32d3623
Shota-Gejima <bewith.sho@gmail.com>
2024-02-20 13:01:56 +0900

    [復習]いろいろな配列

--- chapter14/Main.java
+++ chapter14/Main.java
@@ -1,11 +1,8 @@
 public class Main {
   public static void main(String[] args){
-    // コード14-1
     Empty e = new Empty();
-    String s = e.toString();
-    System.out.println(s);
-    // コード14-2
-    Object o1 = new Empty();
-    Object o2 = "こんにちわ";
+    e.name = "ミナト";
+    e.hp = 100;
+    System.out.println(e.toString());
   }
 }
--- practice/Main.java
+++ practice/Main.java
@@ -5,64 +5,48 @@ public class Main {
     // 問題1 以下の整数型の配列が与えられたとき、配列内の奇数の合計を計算して表示するJavaのプログラムを書いてください。
     // int[] numbers = {1, 5, 8, 3, 6, 9, 7, 2, 4, 11};
     int[] numbers = {1, 5, 8, 3, 6, 9, 7, 2, 4, 11};
-    int numbersAdd=0;
-    for (int i=0; i<numbers.length; i++){
-      System.out.print(numbers[i]);
-      if (i<(numbers.length-1)){
-        System.out.print(",");
-      }
-    }
+    int totalOddNumber = 0;
     for (int i=0; i<numbers.length; i++){
       if (numbers[i]%2!=0){
-        numbersAdd += numbers[i];
+        totalOddNumber += numbers[i];
       }
     }
-    Arrays.sort(numbers);
-    System.out.println();
-    for (int i=0; i<numbers.length; i++){
-      System.out.print(numbers[i]);
-      if (i<(numbers.length-1)){
-        System.out.print(",");
-      }
-    }
-    System.out.println();
-    System.out.println(numbersAdd);
+    System.out.println(totalOddNumber);
+    
     // 問題2 ユーザーに10個の整数を入力させ、それらの整数の合計と平均を計算して表示するJavaのプログラムを書いてください。
-    int[] num = new int[10];
-    int total=0;
-    double avg =0;
-    // for (int i=0; i<num.length; i++){
+    int userNum [] = new int [10];
+    int total = 0;
+    // for (int i=0; i<userNum.length; i++){
       // if (i!=9){
-        // System.out.println("あと"+(9-i)+"回入力してください");
-        // num[i] = new Scanner(System.in).nextInt();
+        // System.out.println(i+1+"回目の数字を入力してください");
+        // userNum[i] = new Scanner(System.in).nextInt();
+      // } else {
+        // System.out.println("ラスト!"); 
+        // userNum[i] = new Scanner(System.in).nextInt();
       // }
     // }
-    // for (int i=0; i<num.length; i++){
-      // total += num[i];
-      // avg = (double)total/num.length;
+    // for (int i=0; i<userNum.length; i++){
+      // System.out.print(userNum[i]);
+      // if (i!=10){
+        // System.out.print(",");
+      // }/
+      // total += userNum[i];
     // }
-    // System.out.println("合計は"+total+"で、平均は"+avg);
+    // System.out.println("合計は"+total);
+    // System.out.println("平均は"+(double)total/userNum.length);
+
+    // System.out.println();
 
     // 問題3 以下の文字列型の配列が与えられたとき、配列内の文字列の中で最も長い文字列を表示するJavaのプログラムを書いてください。
     // String[] words = {"apple", "banana", "kiwi", "strawberry", "grape"};
     String[] words = {"apple", "banana", "kiwi", "strawberry", "grape"};
-    String word = "";
-    for (int i=0; i<words.length; i++){
-      if (word.length()<words[i].length()){
-        word = words[i];
-      } 
-    }
-    System.out.println(word);
-    // 問題4
-    String[] word1 = new String[3];
-    word1[0] = "{1,2,3}";
-    word1[1] = "{2,3,4}";
-    word1[2] = "{3,4,5}";
-    for (int i=0; i<word1.length; i++){
-      System.out.print(word1[i]);
-      if (i<word1.length-1){
-        System.out.print(",");
+    String fruits = "";
+    for (int i= 0; i<words.length; i++){
+      if (fruits.length()<words[i].length()){
+        fruits = words[i];
       }
     }
+    System.out.println(fruits);
+
   }
 }
